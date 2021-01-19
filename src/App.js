c864de3657721f90896f3d542ccb179734651f37
l4kk3r <66743739+l4kk3r@users.noreply.github.com>
2021-01-19 22:40:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import {BrowserRouter, Switch, Route, useHistory, Redirect} from 'react-router-dom'
-import Home from './components/Home'
-import Admin from './components/Admin'
+import React, {useEffect, useState} from 'react'
+import './styles.css'
 
-const Routing = () => {
-  return(
-    <Switch>
-      <Route path='/' component = {Home} exact />
-      <Route path='/admin' component = {Admin} exact />
-    </Switch>
-  )
+const Admin = () => {
+    const [tasks, setTasks] = useState([])
+    const [newtasking, setNewTasking] = useState([])
+    useEffect(()=>{
+        fetch('https://tasks52.herokuapp.com/all-tasks',{method: "get", headers: {"mode":"no-cors", "Access-Control-Allow-Origin": "*"}}).then(ans=> ans.json()).then(realans=> setTasks(realans.tasks))
+    }, [])
+
+    const deleteTasking = (task) => {
+        fetch('https://tasks52.herokuapp.com/change-task',{method: "post", headers: {"mode":"no-cors", "Content-Type":"application/json", "Access-Control-Allow-Origin": "*"}, body: JSON.stringify({id: 1, event: 'delete', task: task})}).then(ans=> ans.json()).then(realans=> console.log(realans))
+        setTasks(tasks.filter(item => item !== task))
+    }
+
+    const addNewTasking = () => {
+        fetch('https://tasks52.herokuapp.com/change-task',{method: "post", headers: {"mode":"no-cors", "Content-Type":"application/json", "Access-Control-Allow-Origin": "*"}, body: JSON.stringify({id: 1, event: 'add', task: newtasking})}).then(ans=> ans.json()).then(realans=> console.log(realans))
+        setTasks(old=>[...old, newtasking])
+        setNewTasking("")
+    }
+
+    return (
+        <div className='aroot'>
+            <div class='admin'>
+                <div class='tasking'>
+                  {tasks.map(task=> <div class='intasking'><h3>{task}</h3><button onClick={() => deleteTasking(task)}>Удалить</button></div>)}
+                </div>
+                <div class='add-tasking'>
+                    <input class='adminput' onChange={(e) => setNewTasking(e.target.value)} value={newtasking} type='text' />
+                    <button class='adminbutton' onClick={() => addNewTasking()}>Ок</button>
+                </div>
+            </div>
+        </div>
+
+    )
 }
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routing />
-    </BrowserRouter>
-  );
-}
-
-export default App;
+export default Admin;
